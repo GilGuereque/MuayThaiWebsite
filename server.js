302ef52b55c43e1c0a1fee6d36af7b77e271074b
GilGuereque <gguereque326@gmail.com>
2023-09-19 21:05:41 -0700

    Installed express and converting to express app in server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+// Set up express app variables & PORT
+const express = require('express');
+const app = express();
+const PORT = 5050;
+
+// Set up mongoose & config database connection
+// const mongoose = require('mongoose');
+// const passport = require('passport');
+// const connectDB = require('./config/database')
+
+// Setting up middleware for Express
+app.set('view engine', 'html')
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Initial GET request
+app.get('/', (req,res) => {
+    try {
+        res.render('index.html')
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('500 HTTP Status code. A server error has occurred from the GET request.');
+    }
+});
+
+// Set up PORT connection
+app.listen(PORT || process.env.PORT, () => {
+    console.log(`The server is running on PORT ${PORT}`);
+});
