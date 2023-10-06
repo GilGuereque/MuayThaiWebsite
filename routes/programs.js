0680e6ffc1c6b247ae020a9694e5bb2689d0ed00
GilGuereque <gguereque326@gmail.com>
2023-10-05 20:39:08 -0700

    removing /programs and replacing it with '/' so programs isn't doubled

--- routes/programs.js
+++ routes/programs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const programsController = require('../controllers/programs');
 
 // Route to GET call in controller
-router.get('/programs', programsController.getPrograms);
+router.get('/', programsController.getPrograms);
 
 // Export Module
 module.exports = router;
